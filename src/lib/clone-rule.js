e4294157633cf7133587f50a46d074249df30b09
Andrew Low <contact@ackl.me>
2021-06-19 18:18:58 +0800

    use dir attr selector instead of pseudoselector
    * better browser support
    * remove need for postcss-dir-pseudo-class peer dep

--- src/lib/clone-rule.js
+++ src/lib/clone-rule.js
@@ -1,11 +1,18 @@
-import postcss from 'postcss';
+import postcss from "postcss";
 
 export default (decl, dir) => {
-	const rule = Object(decl.parent).type === 'rule' ? decl.parent.clone({
-		raws: {}
-	}).removeAll() : postcss.rule({ selector: '&' });
+	const rule =
+		Object(decl.parent).type === "rule"
+			? decl.parent
+					.clone({
+						raws: {},
+					})
+					.removeAll()
+			: postcss.rule({ selector: "&" });
 
-	rule.selectors = rule.selectors.map(selector => `${selector}:dir(${dir})`);
+	rule.selectors = rule.selectors.map(
+		(selector) => `[dir="${dir}"] ${selector}`
+	);
 
 	return rule;
 };
